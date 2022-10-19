21cf1baa93885a6e20263fa149364cb509cb8207
Samir Jafferali <me@samir.id>
2022-10-18 20:42:59 -0700

    Add timeout

--- helpers/relay.py
+++ helpers/relay.py
@@ -6,6 +6,6 @@ def send_webhook(data):
     rheaders = {
         "Content-Type": "application/json",
     }
-    r = requests.post(WEBHOOK_URL, json=data, headers=rheaders)
+    r = requests.post(WEBHOOK_URL, json=data, headers=rheaders, timeout=5)
     r.raise_for_status()
     
