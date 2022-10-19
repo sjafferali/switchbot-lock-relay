77a45907b420f75371251444b9e29859b84399d3
Samir Jafferali <me@samir.id>
2022-10-19 09:50:58 -0700

    Set 10 second timeout instead of 5

--- helpers/relay.py
+++ helpers/relay.py
@@ -1,11 +1,12 @@
 import requests
 from cfg import WEBHOOK_URL
 
+TIMEOUT=10
 
 def send_webhook(data):
     rheaders = {
         "Content-Type": "application/json",
     }
-    r = requests.post(WEBHOOK_URL, json=data, headers=rheaders, timeout=5)
+    r = requests.post(WEBHOOK_URL, json=data, headers=rheaders, timeout=TIMEOUT)
     r.raise_for_status()
     
