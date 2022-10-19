fd21d3fa89bc631f5187155d0df832c6a3be8946
Samir Jafferali <me@samir.id>
2022-10-18 18:45:26 -0700

    Remove debug line

--- conn.py
+++ conn.py
@@ -4,4 +4,3 @@ from cfg import TOKEN, SECRET
 from switchbot import SwitchBot
 
 switchbot = SwitchBot(token=TOKEN, secret=SECRET, nonce=str(uuid.uuid4()))
-print (f"token {TOKEN}, secret {SECRET}")
